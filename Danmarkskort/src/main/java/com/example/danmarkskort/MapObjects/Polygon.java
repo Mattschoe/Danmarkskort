8264ddf847726b43321130966449baf44991c1c1
ebll <ebll@itu.dk>
2025-03-19 12:15:21 +0100

    Ændringer i parserTEst og Polygon

--- Danmarkskort/src/main/java/com/example/danmarkskort/MapObjects/Polygon.java
+++ Danmarkskort/src/main/java/com/example/danmarkskort/MapObjects/Polygon.java
@@ -9,7 +9,7 @@ public class Polygon implements Serializable {
     //region fields
     private List<Node> nodes;
     private Set<Line> lines;
-    private String type; //The type of polygon, fx: "Building", "Coastline", etc.
+    private String type= ""; //The type of polygon, fx: "Building", "Coastline", etc.
     //endregion
 
     /**
@@ -17,10 +17,14 @@ public class Polygon implements Serializable {
      * @param nodes the collection of nodes belonging to the Polygon
      */
     public Polygon(List<Node> nodes, String type) {
+
         assert nodes.size() != 1;
         this.nodes = nodes;
         lines = new HashSet<>();
         createLines();
+        if(type != null){
+            this.type=type;
+        }
     }
 
     /**
