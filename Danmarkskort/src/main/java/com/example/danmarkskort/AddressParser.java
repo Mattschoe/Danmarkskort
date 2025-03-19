14b647d07041e7516e8c57110f752c1b5b09098e
Therese Schultz-Nielsen <thsch@itu.dk>
2025-03-19 11:30:50 +0100

    Ny regex optimus maximus

--- Danmarkskort/src/main/java/com/example/danmarkskort/AddressParser.java
+++ Danmarkskort/src/main/java/com/example/danmarkskort/AddressParser.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 public class AddressParser {
 
     String[] address; // street, house, postcode, city
-    private final static String REGEX = "(?<street>[0-9]*[A-Za-zæøåÆØÅ\\\\. ]+) (?<house>[0-9]+[A-Z]?)(,)*(?<floorandside>.)* *(?<postcode>[0-9]{4}) (?<city>[A-Za-zÆÅØåæø ]+)";
+    private final static String REGEX = "(?<fulladdress>(?<street>[0-9]*[A-Za-zæøåÆØÅ. ]+) (?<house>[0-9]+[A-Z]?)(,)*(?<floorandside>.)* *(?<postcode>[0-9]{4}) (?<city>[A-Za-zÆÅØåæø ]+))|(?<roadNumberCity>(?<street2>[0-9]*[A-Za-zæøåÆØÅ. ]+) (?<house2>[0-9]+[A-Z]?)(,)* (?<city2>[A-Za-zÆÅØåæø ]+))|(?<roadHouseNo>(?<street3>[0-9]*[A-Za-zæøåÆØÅ. ]+) (?<house3>[0-9]+[A-Z]?))|(?<roadOrCityOnly>(?<street4>[0-9]*[A-Za-zæøåÆØÅ. ]+))|(?<postcodeOnly>(?<postcode1>[0-9]{4}))";
     private final static Pattern PATTERN = Pattern.compile(REGEX);
 
     public void parseAddress(String input) {
