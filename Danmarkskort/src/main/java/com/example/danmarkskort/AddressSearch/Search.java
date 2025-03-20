dc83a89e995bd815c60165aaf984c6ae357a8976
Matthias Nielsen <mscn@itu.dk>
2025-03-20 11:24:36 +0100

    Alle noder kommer nu ind i deres respektiv gade!

--- Danmarkskort/src/main/java/com/example/danmarkskort/AddressSearch/Search.java
+++ Danmarkskort/src/main/java/com/example/danmarkskort/AddressSearch/Search.java
@@ -5,10 +5,16 @@ import com.example.danmarkskort.MapObjects.Node;
 import java.util.*;
 
 public class Search {
-    Street[] streets;
+    ///A list of streets (no duplicates) that the parser has parsed
+    private Street[] streets;
 
-    public Search(Map<Long, Node> unsortedNodes) {
-        Set<String> streetNames = initializeAllStreetNames(unsortedNodes.values());
+    /**
+     * Saves all nodes in their given street via a sorted list of {@link Street}'s. Get this list via {@link #getStreets()} and search for a given street via a search algorithm
+     * @param nodesWithStreetAddresses all nodes found with a street address. This is calculated in {@code Model}
+     */
+    public Search(Set<Node> nodesWithStreetAddresses) {
+        //Gets all street names and saves them in a set
+        Set<String> streetNames = initializeAllStreetNames(nodesWithStreetAddresses);
         streets = new Street[streetNames.size()];
 
         //Creates all the streets and puts them in the streets array
@@ -18,9 +24,9 @@ public class Search {
             i++;
         }
 
-        //Sorts array and puts all nodes in the respectively array
+        //Sorts array and puts all nodes in the respectively Street
         sortAddresses();
-        putNodeInStreets();
+        putNodeInStreets(nodesWithStreetAddresses);
     }
 
     /**
@@ -31,11 +37,7 @@ public class Search {
     private Set<String> initializeAllStreetNames(Collection<Node> nodes) {
         Set<String> result = new HashSet<>();
         for (Node node : nodes) {
-            try {
-                result.add(node.getAddress()[3]);
-            } catch (NullPointerException e) { //Node doesn't have an address
-
-            }
+            result.add(node.getAddress()[3]);
         }
         return result;
     }
@@ -46,19 +48,33 @@ public class Search {
     private void sortAddresses() {
         MergeSort mergeSort = new MergeSort();
         mergeSort.sort(streets);
-
         streets = mergeSort.getSortedArray();
-
-        //TESTING
-        for (int i = 0; i < streets.length; i++) {
-            System.out.println(streets[i]);
-        }
+        System.out.println(streets.length);
     }
 
     /**
      * Puts the {@link Node} in the relevant address
      */
-    private void putNodeInStreets() {
+    private void putNodeInStreets(Collection<Node> nodes) {
+        //This is horrible and should be replaced later with binary search or another optimization algorithm
+        for (int i = 0; i < streets.length; i++) {
+            for (Node node : nodes) {
+                if (streets[i].getStreetName().equals(node.getAddress()[3])) streets[i].addNode(node);
+            }
+        }
 
+        for (int i = 0; i < streets.length; i++) {
+            System.out.println(streets[i].getStreetName());
+            for (Node node : streets[i].getNodes()) {
+                System.out.println(node.getAddress()[1] + " " + node.getAddress()[0]);
+            }
+            System.out.println();
+        }
     }
+
+    //region getters and setters
+    public Street[] getStreets() {
+        return streets;
+    }
+    //endregion
 }
