724de16da70de6e8f97a999eaa7bccb2ecdce9cb
oshe <oshe@itu.dk>
2025-05-07 10:04:58 +0200

    PDFOutput fikset og ekstra testet

--- Danmarkskort/src/test/java/com/example/danmarkskort/PDFOutputTest.java
+++ Danmarkskort/src/test/java/com/example/danmarkskort/PDFOutputTest.java
@@ -2,9 +2,11 @@ package com.example.danmarkskort;
 
 import com.example.danmarkskort.MapObjects.Node;
 import com.example.danmarkskort.MapObjects.Road;
+import com.itextpdf.text.DocumentException;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PDFOutputTest {
 
-    @Test
-    public void createValidPdfTest() {
+    /// Tests that PDFOutput's generateRoute-function creates a PDF given valid input
+    @Test public void createValidPdfTest() {
         Node node_1 = new Node(1, 1);
         Node node_2 = new Node(1, 2);
         Node node_3 = new Node(2, 2);
@@ -22,15 +24,15 @@ public class PDFOutputTest {
         List<Node> nodes_2 = List.of(node_2, node_3);
 
         Road road_1 = new Road(nodes_1, true, true, true, "primary", "Road 1");
-        Road road_3 = new Road(nodes_1, true, true, true, "primary", "Road 1");
+        Road road_3 = new Road(nodes_1, true, true, true, "primary", "Road 3");
         Road road_2 = new Road(nodes_2, true, true, true, "primary", "Road 2");
 
-        List<Road> route = List.of(road_1, road_3, road_2);
+        List<Road> route = List.of(road_2, road_1, road_3);
 
         try {
             PDFOutput.generateRoute(route, false);
         } catch (Exception e) {
-            System.out.println(e.getClass() +" --> "+ e.getMessage());
+            System.out.println(e +": "+ e.getMessage());
         }
 
         File file = new File("output/Road-1-to-Road-2.pdf");
@@ -38,21 +40,20 @@ public class PDFOutputTest {
         assertTrue(file.delete());
     }
 
-    @Test
-    public void createInvalidPdfTest() {
+    /// Tests that PDFOutput's generateRoute-function throws an exception on invalid input
+    @Test public void createInvalidPdfTest() {
         List<Node> nodes_1 = new ArrayList<>();
-        nodes_1.add(new Node(1, 1));
-        nodes_1.add(new Node(1, 2));
+        nodes_1.add(new Node(50, 50));
+        nodes_1.add(new Node(65, 17));
 
         List<Node> nodes_2 = new ArrayList<>();
-        nodes_2.add(new Node(1, 2));
-        nodes_2.add(new Node(2, 2));
+        nodes_2.add(new Node(30, 60));
+        nodes_2.add(new Node(30, 89));
 
         Road road_1 = new Road(nodes_1, true, true, true, "primary", "");
-        Road road_2 = new Road(nodes_2, true, true, true, "primary", "Road 2");
-        Road road_3 = new Road(nodes_2, true, true, true, "primary", "");
+        Road road_2 = new Road(nodes_2, true, true, true, "primary", "   ");
 
-        List<Road> route = List.of(road_1, road_2, road_3);
+        List<Road> route = List.of(road_1, road_2);
 
         assertThrows(RuntimeException.class, () ->
             PDFOutput.generateRoute(route, false)
@@ -61,5 +62,31 @@ public class PDFOutputTest {
         File file = new File("output/Nameless-place-to-nameless-place.pdf");
         assertTrue(file.exists());
         assertTrue(file.delete());
+    }
+
+    /// Tests that right/left turns are processed correctly in all four different vector-arrangements
+    @Test public void turnWorksInAllFourCasesTest() {
+        Node node_1 = new Node(1, 1);
+        Node node_2 = new Node(1, 2);
+        Node node_3 = new Node(2, 2);
+
+        assertDoesNotThrow(() -> testTurn(node_1, node_2, node_2, node_3));
+        assertDoesNotThrow(() -> testTurn(node_1, node_2, node_3, node_2));
+        assertDoesNotThrow(() -> testTurn(node_2, node_1, node_3, node_2));
+        assertDoesNotThrow(() -> testTurn(node_2, node_1, node_2, node_3));
+    }
+
+    /// Function tests whether a left turn is correctly assessed given an arrangement of nodes
+    private static void testTurn(Node node_1, Node node_2, Node node_3, Node node_4) throws DocumentException, FileNotFoundException {
+        List<Node> nodes_1 = List.of(node_1, node_2);
+        List<Node> nodes_2 = List.of(node_3, node_4);
+
+        Road road_1 = new Road(nodes_1, true, true, true, "primary", "Road 1");
+        Road road_2 = new Road(nodes_2, true, true, true, "primary", "Road 2");
+
+        List<Road> route = List.of(road_2, road_1);
+
+        String turn = PDFOutput.generateRoute(route, false);
+        assertEquals("Turn left", turn);
     }
 }
